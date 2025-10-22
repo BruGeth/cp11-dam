e3192fef9c15455b4cf0688058d68a69c6912c7c
BruGeth <g15bru03@hotmail.com>
2025-10-22 02:44:21 -0500

    feat: actualizar App.js para incluir imports de Camera y AsyncStorage

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef,useEffect } from 'react';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { Camera } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
