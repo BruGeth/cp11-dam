4cb1541b059fe5df4f8d62d3b653da2d40376580
BruGeth <g15bru03@hotmail.com>
2025-10-22 03:19:46 -0500

    feat: agregar funcionalidad para tomar foto y mostrar imagen

--- App.js
+++ App.js
@@ -31,6 +31,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {!photo ? (
+        <Camera style={{ flex: 1 }} ref={cameraRef} />
+      ) : (
+        <Image source={{ uri: photo }} style={{ flex: 1 }} />
+      )}
+      <Button
+        title={photo ? "Volver a cámara" : "Tomar foto"}
+        onPress={() => (photo ? setPhoto(null) : takePicture())}
+      />
     </View>
   );
 }
